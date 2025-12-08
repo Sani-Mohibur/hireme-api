ff8c53defc5858ede7e7e1a53a490b8fd1b70a18
Sani-Mohibur <mohiburrahmansani@gmail.com>
2025-12-09 04:23:51 +0600

    feat: add .env.example

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI =
-  process.env.MONGO_URI ||
-  "mongodb+srv://sani:sani1234@cluster.ui1dxit.mongodb.net/HireMe?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI!;
 
 mongoose
   .connect(MONGO_URI)
